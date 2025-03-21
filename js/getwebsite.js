32f95520bbfa1725c58048590ddfbe9941664c0a
Admin <gillam96@gmail.com>
2025-03-21 11:04:28 -0500

    updating css style

--- js/getwebsite.js
+++ js/getwebsite.js
@@ -5,11 +5,11 @@ fetch('websiteconfig.json')
     document.getElementById('websiteData').innerHTML = `
       <p><a href="${data.linkedin}">
       <img src="images/linkedinicon.jpg" alt="LinkedIn Page" width="50" height="50" align="left"
-      style="margin-left: 100px;">
+      style="margin-left: 600px;">
       </a></p>
       <p><a href="${data.github}">
       <img src="images/githubicon.png" alt="GitHub Page" width="50" height="50" align="right"
-      style="margin-right: 100px;">
+      style="margin-right: 600px;">
       </a></p>
       <p><a href="${data.resume}">
       <img src="images/cvicon.png" alt="Resume Page" width="40" height="50" align="center">
