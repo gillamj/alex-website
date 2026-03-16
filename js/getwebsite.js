78d82b725c5bcb2106a751a546b1c50a82c0b499
Admin <gillam96@gmail.com>
2026-03-16 17:56:47 -0500

    Updating style.css to adjust website image sizes

--- js/getwebsite.js
+++ js/getwebsite.js
@@ -1,19 +1,25 @@
 function getWebsiteDetails() {
+
   fetch('websiteconfig.json')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('websiteData').innerHTML = `
-        <a href="${data.linkedin}" target="_blank" style="margin-right: 2rem;">
-          <img src="images/linkedinicon.jpg" alt="LinkedIn">
-        </a>
-        <a href="${data.github}" target="_blank" style="margin-right: 2rem;">
-          <img src="images/githubiconv2.png" alt="GitHub">
-        </a>
-        <a href="${data.resume}" target="_blank">
-          <img src="images/cvicon.png" alt="Resume">
-        </a>
-      `;
-    });
+  .then(response => response.json())
+  .then(data => {
+
+    document.getElementById('websiteData').innerHTML = `
+      <a href="${data.linkedin}" target="_blank">
+        <img src="images/linkedinicon.jpg" alt="LinkedIn">
+      </a>
+
+      <a href="${data.github}" target="_blank">
+        <img src="images/githubiconv2.png" alt="GitHub">
+      </a>
+
+      <a href="${data.resume}" target="_blank">
+        <img src="images/cvicon.png" alt="Resume">
+      </a>
+    `;
+
+  });
+
 }
 
-getWebsiteDetails().catch(error => console.log('Error loading JSON:', error));
+getWebsiteDetails();
